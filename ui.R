b2cb89651ecc52dcb020345b8430b78e1f49d174
fintzij <fintzij@uw.edu>
2015-05-17 17:24:57 -0700

    Basic UI for data upload and viewing.

--- ui.R
+++ ui.R
@@ -1,1 +1,44 @@
 # ui.R - User interface for the data exploration and classifier construction tool
+
+library(ggvis)
+library(magrittr)
+
+shinyUI(navbarPage("Exploratory binary classifier construction",
+                   
+         tabPanel("Upload and view data",
+                  sidebarLayout(
+                            sidebarPanel(fileInput('file1', 'Choose file to upload',
+                                                   accept = c(
+                                                             'text/csv',
+                                                             'text/comma-separated-values',
+                                                             'text/tab-separated-values',
+                                                             'text/plain',
+                                                             '.csv',
+                                                             '.tsv'
+                                                            )
+                                                  ),
+                                         tags$hr(),
+                                         checkboxInput('header', 'Header', TRUE),
+                                         radioButtons('sep', 'Separator',
+                                                      c(Comma=',',
+                                                        Semicolon=';',
+                                                        Tab='\t'),
+                                                      ','),
+                                         radioButtons('quote', 'Quote',
+                                                      c(None='',
+                                                        'Double Quote'='"',
+                                                        'Single Quote'="'"),
+                                                      '"'),
+                                         tags$hr()
+                                      ),
+                            
+                            mainPanel(
+                                      tableOutput('contents')
+                                      )
+                            
+                            )
+                  ),
+         tabPanel("Explore data"),
+         tabPanel("Classifier performance")          
+
+          ))
