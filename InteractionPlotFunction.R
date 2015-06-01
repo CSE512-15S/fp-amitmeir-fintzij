76ddceed821f6483a53d1ad39a5fbb0a6cd33993
ammeir2 <amitmeir@Amits-MacBook-Pro.local>
2015-06-01 16:19:08 -0700

    functions for glmnet and viz
    interaction ggvis code and glmnet fitting functions.

---
+++ InteractionPlotFunction.R
@@ -0,0 +1,134 @@
+interactionPlot <- function(varsInModel,data,error) {
+  if(length(varsInModel==0)) return(NULL)
+  
+  #Computing correlations with error
+  varsInModel <- sort(varsInModel)
+  nInteractions <- choose(length(varsInModel),2)+length(varsInModel)
+  varNameLevels <- c(varsInModel,sample(varsInModel,nInteractions-length(varsInModel),replace=TRUE))
+  interactions <- data.frame(var1=varNameLevels,
+                             var2=varNameLevels,
+                             errorCorrelation=numeric(nInteractions))
+  count <- 0
+  for(i in 1:length(varsInModel)) {
+    for(j in i:length(varsInModel)) {
+      count <- count + 1
+      generateNewVar <- paste("with(data,",varsInModel[i],"*",varsInModel[j],")")
+      newVar <- eval(parse(text=generateNewVar))
+      interactions[count,1:2] <- c(varsInModel[i],varsInModel[j])
+      interactions[count,3] <- cor(error,newVar,method="spearman")
+    }
+  }
+  
+  #interactions[,3] <- runif(min=-1,max=1,nrow(interactions))
+  
+  interactions$roundCor <- round(interactions$errorCorrelation,2)
+  
+  ##Setting up correlations colors
+  Blue = colorRampPalette(c("blue","grey"))
+  Red = colorRampPalette(c("grey","red"))
+  
+  # Negative values of defense get a blue color scale with 10 colors
+  interactions$def.color[interactions$roundCor<0] = 
+    as.character(cut(interactions$roundCor[interactions$roundCor<0], 
+                     seq(-1.1, 0, length.out=21), 
+                     labels=Blue(20)))
+  
+  # Positive values of defense get an orange color scale with 10 colors
+  interactions$def.color[interactions$roundCor>=0] = 
+    as.character(cut(interactions$roundCor[interactions$roundCor>=0], 
+                     seq(-0.1,1.1,length.out=21), 
+                     labels=Red(20)))
+  
+  #tooltip function
+  interactionToolTip <- function(x) {
+    if(is.null(x)) return(NULL)
+    row <- interactions[interactions$id == x$id, ]
+    paste(row$var1,"X",row$var2,"\n","Error Correlation: ",round(row$errorCorrelation,2))
+  }
+  
+  clickToolTip <- function(x) {
+    if(is.null(x)) return(NULL)
+    row <- interactions[interactions$id == x$id, ]
+    print(c(as.character(row$var1),as.character(row$var2)))
+    return(NULL)
+  }
+  
+  interactions$id <- 1:nrow(interactions)
+  interactions$errorCorRound2 <- round(interactions$errorCorrelation,2)
+  
+  ggvisPlot <- ggvis(data=interactions,x=~var1,y=~var2,fill:=~def.color,key:=~id) %>% 
+    #layer_points() %>%
+    layer_rects(width=band(),height=band()) %>% 
+    #layer_rects() %>% 
+    layer_text(text:=~errorCorRound2, stroke:="black",fill:="white", align:="left", baseline:="top",fontSize:=150/length(varsInModel)) %>%
+    scale_nominal("x", padding = 0, points = FALSE) %>% 
+    scale_nominal("y", padding = 0, points = FALSE) %>%
+    add_tooltip(interactionToolTip, "hover") %>%
+    add_tooltip(clickToolTip,"click")
+      
+  return(ggvisPlot)
+}
+
+mainEffectPlot <- function(allVariables,varsInModel,response,data,error=NULL) {
+  #Computing correlations
+  nVars <- length(allVariables)
+  correlations <- data.frame(variable=allVariables,correlation=nVars)
+  for(i in 1:nVars) {
+    if(allVariables[i] %in% varsInModel) {
+      correlations$correlation[i] <- 1
+      } else {
+        commandComputeCor <- "with(data,cor("
+        if(is.null(varsInModel)) {
+        commandComputeCor <- paste(commandComputeCor,response)
+      } else { 
+        commandComputeCor <- paste(commandComputeCor,"error")
+      }
+      commandComputeCor <- paste(commandComputeCor,",",allVariables[i],",method='spearman'))")
+      correlations$correlation[i] <- eval(parse(text=commandComputeCor))
+    }
+  }
+  
+  correlations$roundCor <- round(correlations$correlation,2)
+}
+
+fitGlmnetModel <- function(response,varsInModel,data,lambda=NULL) {
+  if(is.null(varsInModel)) return(NULL)
+    
+  require(glmnet)
+  #Generating design matrix
+  commandDesignMatrix <- "X <- model.frame(~1"
+  for(i in 1:length(varsInModel)) {
+    commandDesignMatrix <- paste(commandDesignMatrix,"+",varsInModel[i])
+  }
+  commandDesignMatrix <- paste(commandDesignMatrix,",data=data)")
+  eval(parse(text=commandDesignMatrix))
+  
+  #Fitting Model 
+  commandFitModel <- paste("fit <- cv.glmnet(y=data$",response,",x=as.matrix(X),family='binomial')")
+  eval(parse(text=commandFitModel))
+  
+  if(is.null(lambda)) {
+    prediction <- predict(fit,newx=as.matrix(X),type="response")
+  } else {
+    prediction <- predict(fit,newx=as.matrix(X),type="response",s=lambda)
+  }
+  
+  commandComputeError <- paste("error <- prediction-data$",response)
+  eval(parse(text=commandComputeError))
+  return(list(fit=fit,prediction=prediction,error=error))
+}
+
+# TEST
+# result <- fitGlmnetModel(response,varsInModel,data,lambda=NULL)
+# error <- result$error
+# interactionPlot(varsInModel,data,error)
+
+
+
+
+
+
+
+
+
+
---
+++ basicVizGGVIS.R
@@ -0,0 +1,85 @@
+library(ggvis)
+library(ggplot2)
+require(gam)
+
+generateMainPlot <- function(xVar=NULL,
+                             yVar=NULL,
+                             response=NULL,
+                             data) {
+  #Taking care of possible null variables and creating data set
+  xVarNull <- is.null(xVar)
+  yVarNull <- is.null(yVar)
+  facetXnull <- is.null(facetX)
+  facetYnull <- is.null(facetY)
+  responseNull <- is.null(response)
+  
+  if(xVarNull) xVar <- rep("",numObs)
+  if(xVarNull) xVar <- rep("",numObs)
+  if(facetXnull) facetX <- rep("",numObs)
+  if(facetYnull) facetY <- rep("",numObs)
+  if(responseNull) response <- rep(1,numObs)
+      
+  #If both variables are null return empty plot
+  if(is.null(xVar) & is.null(yVar)) {
+    return(plot)
+  }
+  
+  #If just one is null return a histogram
+  if(is.null(yVar) | is.null(xVar)) {
+    if(is.null(yVar)) {
+      variable <- xVar
+    } else {
+      variable <- yVar
+    }
+    
+    plot <- ggvis(data=data,x=parse(text=paste("~",variable)),y=parse(text=(paste("~as.factor(",response,")")))) %>%
+      layer_boxplots() 
+    
+    #flip coordinates if x is null
+  
+  #If both variables are not null plot scatter plot
+  } else { 
+    plotCommand <- paste("ggvis(data=data,x=~",xVar,",y=~",yVar,",fill=~",response,")")
+    plotCommand <- paste(plotCommand,"%>% layer_points()")
+    eval(parse(text=plotCommand))
+  }
+  
+  #Facet according to facetting variables 
+  plot <- plot + facet_grid(facetY ~ facetX)
+  
+  return(plot)
+}
+
+yVar <- iris$Sepal.Width
+xVar <- iris$Sepal.Length
+facetX <- rbinom(length(yVar),1,0.5)
+facetY <- rbinom(length(yVar),1,0.5)
+response <- iris$Species=="virginica"
+
+
+generateMainPlot(xVar=xVar,xVarName="Sepal Width",
+                 yVar=yVar,yVarName="Sepal Length",
+                 response=response,responseName="Virginica",
+                 numObs=nrow(iris),
+                 predict=predictions,
+                 facetX=facetX,facetXName="randomX",
+                 facetY=facetY,facetYName="randomY")
+
+model <- glm(response ~ Sepal.Length + Sepal.Width,family="binomial",data=data)
+predictions <- predict(model,type="response")
+
+predSmoother <- gam(predictions~s(Sepal.Length)+ s(Sepal.Width),data=data)
+grid <- with(data,expand.grid(x=seq(from=min(Sepal.Length)-0.1*sd(Sepal.Length),to=max(Sepal.Length)+0.1*sd(Sepal.Length),by=sd(Sepal.Length)*0.05),
+                    y=seq(from=min(Sepal.Width)-0.1*sd(Sepal.Width),to=max(Sepal.Width)+0.1*sd(Sepal.Width),by=sd(Sepal.Width)*0.05)))
+smoothed.pred <- predict(predSmoother,newdata=data.frame(Sepal.Length=grid[,1],Sepal.Width=grid[,2]))
+
+predictorData <- data.frame(Sepal.Length=grid[,1],Sepal.Width=grid[,2],smoothed.pred)
+
+ggvis(data=predictorData,x=~Sepal.Length,y=~Sepal.Width,fill=~smoothed.pred,opacity:=0.55,size:=20) %>% layer_points()
+
+ggplot() + geom_tile(aes(x=grid[,1],y=grid[,2],fill=as.vector(smoothed.pred))) + 
+  scale_fill_gradient2(midpoint=0.5,mid="white",high="blue",low="orange") +
+  geom_point(aes(x=xVar,y=yVar,color=response)) + theme_bw()
+
+  
+  
